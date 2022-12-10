94c8d5c81bbb431f395336612b19719b60ae7a5e
Chris Hartford <chartfor@mail.ccsf.edu>
2022-12-09 20:52:19 -0800

    test of control

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather',
+        title: 'WWeather!!!',
         name: 'Chris Hartford'
     })
 })
